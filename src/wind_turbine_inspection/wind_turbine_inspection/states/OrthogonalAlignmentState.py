d39cde80cec8b3f1fd46e51f5c90cc489d58ce73
Bruno Centanaro <bcentanaro@gmail.com>
2024-08-25 18:22:03 -0300

    agrega para hacer un movimiento centrado en el molino
    Former-commit-id: 7dbb0d002202d0c46dc321cab2a1016582f0a0f7

--- src/wind_turbine_inspection/wind_turbine_inspection/states/OrthogonalAlignmentState.py
+++ src/wind_turbine_inspection/wind_turbine_inspection/states/OrthogonalAlignmentState.py
@@ -1,14 +1,12 @@
 from wind_turbine_inspection.states.base import InspectionState, WindTurbineInspectionStage
+from std_msgs.msg import String
 
 class OrthogonalAlignmentState(InspectionState):
     def __init__(self, state_machine):
         super().__init__('orthogonal_alignment_state', WindTurbineInspectionStage.ORTHOGONAL_ALIGNMENT, state_machine)
-        self.timer = self.create_timer(5.0, self.startAfter5Seconds)
-        self.get_logger().info(f"Timer created {self.timer.time_until_next_call()}")
-
-    def startAfter5Seconds(self):
-        self.advance_to_next_state()
-
+        self.moveCenteredPublisher = self.create_publisher(String, 'move_centered', 10)
+        self.moveCenteredPublisher.publish(String(data=""))
 
     def waypoint_reached_callback(self, msg):
         self.get_logger().info(f"OrthogonalAlignmentState received: {msg.data}")
+        self.advance_to_next_state()
