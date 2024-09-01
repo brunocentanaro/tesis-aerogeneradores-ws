2cfa6fae09c8cd6c22fc1acf06b3b47f3c67ec40
Martin Tapia <martintapia24302@gmail.com>
2024-08-31 23:11:59 -0300

    Cree archivo parh_planner para hacer todo secuencialmente ahi
    Former-commit-id: 2ae373f2e259fe7d254e1373b0ddda3fca8e72f6

---
+++ src/path-planner/path-planner/path_planner.py
@@ -0,0 +1,6 @@
+from stl_gen.create_stl import WindTurbine, create_stl
+from shortest_path import shortest_path
+
+def path_planner(windTurbine: WindTurbine, start_position):
+    #create_stl(windTurbine)
+    create_stl()
