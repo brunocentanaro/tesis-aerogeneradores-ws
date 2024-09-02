f7658c4f042f73ff8af16e97613551f18eb5f749
Martin Tapia <martintapia24302@gmail.com>
2024-09-01 23:17:53 -0300

    Cree la funcion path_planner y ya funciona completamente
    Former-commit-id: f0de7dcb5774f736914fff2ab39f70c1b3e99b3a

--- src/path-planner/path-planner/path_planner.py
+++ src/path-planner/path-planner/path_planner.py
@@ -1,9 +1,9 @@
 from stl_gen.create_stl import WindTurbine, create_stl
 from shortest_path import shortest_path_from_stl, plot_best_order, plot_points
 
-def path_planner(windTurbine: WindTurbine=WindTurbine(80, 3, 10, "turbine"), start_position=[0, -30, 30]):
+def path_planner(windTurbine: WindTurbine=WindTurbine(80, 3, 40, "stl_gen/turbine"), start_position=[0, -30, 30]):
     create_stl(windTurbine)
-    traj = shortest_path_from_stl(start_position, windTurbine.stl_name)
+    traj = shortest_path_from_stl(start_position, windTurbine.stl_path)
     plot_best_order(start_position, traj)
     plot_points(traj)
     return traj
