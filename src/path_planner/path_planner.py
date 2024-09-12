4712b4abde0c202abcfc8064bee10c8d2f79c475
Martin Tapia <martintapia24302@gmail.com>
2024-09-12 20:33:55 -0300

    Cambios para vincularlo con la maquina de estados
    Former-commit-id: 5e035ca760886ea5f850d24487af61c8883f9718

--- src/path_planner/path_planner.py
+++ src/path_planner/path_planner.py
@@ -1,18 +1,18 @@
 from stl_gen.create_stl import WindTurbine, create_stl
 from shortest_path import shortest_path_from_stl, plot_best_order, plot_points
 
-def path_planner(windTurbine: WindTurbine=WindTurbine(3, 40, "stl_gen/turbine"), start_position=[0, -30, 30], safe_distance=10):
+def path_planner(windTurbine: WindTurbine=WindTurbine(6, 39, "stl_gen/turbine"), start_position=[-10, 0, 0], safe_distance=0):
     create_stl(windTurbine)
-    start_position = point_from_machine(start_position)
-    traj = shortest_path_from_stl(start_position, windTurbine.stl_path, safe_distance)
+    traj = shortest_path_from_stl(point_from_machine(start_position), safe_distance, windTurbine.stl_path)
     traj = points_to_machine(traj)
     plot_best_order(start_position, traj)
-    plot_points(traj)
+    #plot_points(traj)
+    print(traj)
     return traj
 
 def point_from_machine(point):
-    y, x, z = point
-    return [x, y, -z]
+    x, y, z = point
+    return [y, x, -z]
 
 def points_to_machine(points):
     return [(y, x, -z) for x, y, z in points]
