2b1127bdfe0cfd7130abc6f146a03ae9d17e00ae
martin24302 <martintapia24302@gmail.com>
2024-10-12 21:11:59 -0300

    Cambie la maquina de estados y borre el RotationState
    Former-commit-id: 84fba6ef23b21406269b4181256fc865379b77b0

--- src/wind_turbine_inspection/wind_turbine_inspection/mission_state_handler.py
+++ src/wind_turbine_inspection/wind_turbine_inspection/mission_state_handler.py
@@ -2,7 +2,6 @@ import rclpy
 from rclpy.node import Node
 from wind_turbine_inspection.states.ApproachState import ApproachState
 from wind_turbine_inspection.states.RegistrationState import RegistrationState
-from wind_turbine_inspection.states.RotationState import RotationState
 from wind_turbine_inspection.states.IdleState import IdleState
 from wind_turbine_inspection.states.OrthogonalAlignmentState import OrthogonalAlignmentState
 from wind_turbine_inspection.states.ReturnHomeState import ReturnHomeState
@@ -13,7 +12,15 @@ class WindTurbineInspectionStateMachine(Node):
         super().__init__('inspection_state_machine')
         self.current_state = IdleState(self)
         self.completedInspectionRounds = 0
+        self.inspectionDistance = 10
         self.spin_until_state_complete()
+    
+    def recalculateInspectionRoundsAndDistance(self):
+        self.completedInspectionRounds = (self.completedInspectionRounds + 1) % 4
+        if self.completedInspectionRounds < 2:
+            self.distance = 10
+        else:
+            self.distance = 20
 
     def spin_until_state_complete(self):
         future = self.current_state.get_future()
@@ -23,9 +30,16 @@ class WindTurbineInspectionStateMachine(Node):
     def change_state(self):
         current_state = type(self.current_state)
         self.current_state.destroy_node()
-    
+        if self.completedInspectionRounds < 2:
+            self.distance = 10
+        else:
+            self.distance = 20
+
         if current_state is IdleState:
-            self.current_state = TakeoffState(self)
+            if self.completedInspectionRounds % 2 == 0:
+                self.current_state = TakeoffState(self)
+            else:
+                self.current_state = RegistrationState(self)
         elif current_state is TakeoffState:
             self.current_state = ApproachState(self)
         elif current_state is ApproachState:
@@ -33,15 +47,12 @@ class WindTurbineInspectionStateMachine(Node):
         elif current_state is OrthogonalAlignmentState:
             self.current_state = RegistrationState(self)
         elif current_state is RegistrationState:
-            if self.completedInspectionRounds < 4:
-                self.current_state = RotationState(self)
-            else:
+            self.recalculateInspectionRoundsAndDistance()
+            if self.completedInspectionRounds % 2 == 0:
                 self.current_state = ReturnHomeState(self)
-        elif current_state is RotationState:
-            self.current_state = RegistrationState(self)
+            else:
+                self.current_state = IdleState(self)
         elif current_state is ReturnHomeState:
-            self.current_state = IdleState(self)
-        else:
             self.current_state = IdleState(self)
 
         self.spin_until_state_complete()
