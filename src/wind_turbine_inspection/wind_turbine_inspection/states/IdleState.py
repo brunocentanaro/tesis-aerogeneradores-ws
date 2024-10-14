af2a7c058622f786cde59161c4dd0132cb8fc829
Bruno Centanaro <bcentanaro@gmail.com>
2024-10-13 23:25:31 -0300

    adds front inspection variable

--- src/wind_turbine_inspection/wind_turbine_inspection/states/IdleState.py
+++ src/wind_turbine_inspection/wind_turbine_inspection/states/IdleState.py
@@ -1,16 +1,24 @@
 from wind_turbine_inspection.states.base import InspectionState, WindTurbineInspectionStage
-from std_srvs.srv import Trigger
+
 
 class IdleState(InspectionState):
     def __init__(self, state_machine):
         super().__init__('idle_state', WindTurbineInspectionStage.IDLE, state_machine)
         self.declare_parameter('mission_param', 0)
-        mission_param = self.get_parameter('mission_param').get_parameter_value().integer_value
+        self.declare_parameter('front_inspection', 1)
+        mission_param = self.get_parameter(
+            'mission_param').get_parameter_value().integer_value
+        isFrontInspectionParam = self.get_parameter(
+            'front_inspection').get_parameter_value().integer_value == 1
         self.update_shared_state('mission_param', mission_param)
+        self.update_shared_state('is_front_inspection', isFrontInspectionParam)
+        self.get_logger().info(f"Front inspection: {isFrontInspectionParam}")
 
         self.get_logger().info(f"Mission parameter: {mission_param}")
         self.get_logger().info("IdleState created")
         self.srv = self.create_service(Trigger, 'comenzar_inspeccion', self.comenzar_inspeccion_callback)
+        self.get_logger().info(
+            f"Timer created {self.timer.time_until_next_call()}")
 
     def comenzar_inspeccion_callback(self, request, response):
         self.advance_to_next_state()
