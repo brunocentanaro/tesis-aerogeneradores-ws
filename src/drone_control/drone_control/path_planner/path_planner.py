08f668af3f5a76f8e39481351f80b0003851d1b8
Martin Tapia <martintapia24302@gmail.com>
2024-10-22 20:36:37 -0300

    comento graficas del path planner
    Former-commit-id: c1cb5b8375b41fdccb2f1c89e8886e6bdb421e2c

--- src/drone_control/drone_control/path_planner/path_planner.py
+++ src/drone_control/drone_control/path_planner/path_planner.py
@@ -4,8 +4,8 @@ from drone_control.path_planner.shortest_path import shortest_path_from_stl, plo
 def path_planner(windTurbine:WindTurbine=WindTurbine(6, 39, "stl_gen/turbine"), start_position=[-10, 0, 0], end_position=None, safe_distance=0, angle_y=0, angle_z=0):
     create_stl(windTurbine)
     traj = shortest_path_from_stl(point_from_machine(start_position), point_from_machine(end_position), safe_distance, angle_y, angle_z, windTurbine.stl_path)
-    points_only = [item[1] for item in traj]
-    plot_best_order(points_only)
+    #points_only = [item[1] for item in traj]
+    #plot_best_order(points_only)
     #plot_points(points_only)
     return result_to_machine(traj[1:])
 
