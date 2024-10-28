9bc71c2b1c3e8f6f72e6080c9d5f264d3c24c206
Bruno Centanaro <bcentanaro@gmail.com>
2024-10-28 18:28:36 -0300

    fixes back inspection not working correctly

--- tests/summary.py
+++ tests/summary.py
@@ -17,27 +17,24 @@ def process_json_files(folder_path):
     for filename in os.listdir(folder_path):
         if filename.endswith('.json'):
             file_path = os.path.join(folder_path, filename)
-            try:
-                with open(file_path, 'r') as file:
-                    data = json.load(file)
-                    position_errors = data.get('positionErrors', [])
-                    duration = data.get('duration', 0)
-                    sim_duration = data.get('simDuration', 0)
-                    if duration:
-                        all_durations.append(duration)
-                    if sim_duration:
-                        all_sim_durations.append(sim_duration)
-                    if position_errors:
-                        median = np.median(position_errors)
-                        std_dev = np.std(position_errors)
-                        medians.append(median)
-                        standard_deviations.append(std_dev)
-                        all_position_errors.extend(position_errors)
-                    else:
-                        print(
-                            f"The file {filename} does not contain 'positionErrors' or it is empty.\n")
-            except json.JSONDecodeError as e:
-                print(f"Error decoding JSON in file {filename}: {e}")
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                position_errors = data.get('positionErrors', [])
+                duration = data.get('duration', 0)
+                simTime = data.get('simDuration', 0)
+                if simTime:
+                    all_sim_durations.append(simTime)
+                if duration:
+                    all_durations.append(duration)
+                if position_errors:
+                    median = np.median(position_errors)
+                    std_dev = np.std(position_errors)
+                    medians.append(median)
+                    standard_deviations.append(std_dev)
+                    all_position_errors.extend(position_errors)
+                else:
+                    print(
+                        f"The file {filename} does not contain 'positionErrors' or it is empty.\n")
 
     if all_position_errors:
         total_median = np.median(all_position_errors)
@@ -45,12 +42,13 @@ def process_json_files(folder_path):
         print("Combined Results from All Files:")
         print(f"Total Median: {total_median}")
         print(f"Total Standard Deviation: {total_std_dev}\n")
-        print(f"Total Duration: {sum(all_durations)}")
-        print(f"Total Duration Median: {np.median(all_durations)}")
-        print(f"Total standard deviation of durations: {np.std(all_durations)}\n")
-        print(f"Total Sim Duration: {sum(all_sim_durations)}")
-        print(f"Total Sim Duration Median: {np.median(all_sim_durations)}")
-        print(f"Total standard deviation of Sim Durations: {np.std(all_sim_durations)}\n")
+        print(f"Median Duration: {np.median(all_durations)}")
+        print(
+            f"Total standard deviation of durations: {np.std(all_durations)}\n")
+
+        print(f"Median Sim Duration: {np.median(all_sim_durations)}")
+        print(
+            f"Total standard deviation of sim durations: {np.std(all_sim_durations)}")
     else:
         print("No 'positionErrors' data found in any file.")
 
